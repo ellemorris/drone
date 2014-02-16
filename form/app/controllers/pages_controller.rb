159bda2078c3bfd43abe54aacce9d0ff7258e405
Harris Gani <harrisgani13@gmail.com>
2014-02-15 23:39:12 -0500

    add controller for pages

--- form/app/controllers/pages_controller.rb
+++ form/app/controllers/pages_controller.rb
@@ -1,2 +1,16 @@
+require 'csv'
+
 class PagesController < ApplicationController
+  def new
+  end
+
+  def create
+    params[:stock_name]
+
+    CSV.open("assets/list.csv", 'w') do |writer|
+      csv << [params[:stock_name]]
+    end
+    # p params[:stock_name]
+    redirect_to "http://localhost:5631"
+  end
 end
