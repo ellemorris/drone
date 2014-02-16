292fe10e4d65b5ea748e62d3315c5e2e1f2f8ab5
Ashwin Muthiah <muthiah.ashwin2015@gmail.com>
2014-02-15 22:50:13 -0500

    form with link button

---
+++ form/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Form::Application.config.secret_token = '9053f307164d8b7880106e76734f277368c09b063054fddae98bd20788a22b907130ad64a154f9aac15be0acec7df904596b36a6c2c8679568ade3a27999ad68'
