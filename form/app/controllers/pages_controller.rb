b5f3a3ea747496407300de47d1405b2ff05359b9
Harris Gani <harrisgani13@gmail.com>
2014-02-15 23:54:58 -0500

    commit before merge

--- form/app/controllers/pages_controller.rb
+++ form/app/controllers/pages_controller.rb
@@ -7,8 +7,8 @@ class PagesController < ApplicationController
   def create
     params[:stock_name]
 
-    CSV.open("assets/list.csv", 'w') do |writer|
-      csv << [params[:stock_name]]
+    File.open("list.txt", 'w') do |writer|
+      writer.write(params[:stock_name])
     end
     # p params[:stock_name]
     redirect_to "http://localhost:5631"
